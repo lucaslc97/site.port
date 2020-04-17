192bde99428b73bd9bf7698a498fbe838cfff176
Lucas Cabral <49406931+lucaslc97@users.noreply.github.com>
2020-04-17 01:07:01 -0300

    Parte 2

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+botao = document.getElementById("botao-hamb")
+
+botao.addEventListener("click", clicou)
+
+
+function clicou() {
+    
+}
+
+function btnMenu() {
+
+    menu = document.querySelectorAll("#botoes a")
+
+    menu.style.display = "block"
+    console.log(menu)
+
+
+}
